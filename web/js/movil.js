4d03349eb0e321641c5b36331d6da827323dce01
Pascual Maestre Server <pascual.maestreserver@gmail.com>
2016-01-07 20:10:23 +0100

    Listar alquileres
    Añadir código javaScript.
    Terminada la pantalla para mostrarlo.

--- web/js/movil.js
+++ web/js/movil.js
@@ -30,6 +30,39 @@ function listarSalas(){
 	xhr.send();
 }
 
+
+/////////////////////////////// ALQUILERES ///////////////////////////////
+
+function listarAlquileres(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', URL+"/api/alquileres", true);
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4 && xhr.status == 200)
+		{
+			var alquileres = JSON.parse(xhr.responseText);
+			for (var i = 0; i < alquileres.rows.length; i++) {
+				if(i == 0)
+					document.getElementById("listaAlquileres").innerHTML = '<li id="id_'+alquileres.rows[i].id+'">Alquiler '+alquileres.rows[i].id+': '+alquileres.rows[i].fechaIni+'-'+alquileres.rows[i].fechaFin+' '+alquileres.rows[i].comentario+' '+
+                     '<a href="javascript:deleteAlquileres('+alquileres.rows[i].id+')class="ui-btn ui-btn-inline ui-btn-raised waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-delete zmd-2x"></i></a> ' +
+                     '<a href="javascript:verDetallesAlquileres('+alquileres.rows[i].id+')class="ui-btn ui-mini ui-btn-inline ui-btn-icon-block waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-edit zmd-2x"></i></a> ' +  
+                     '</li>';
+                else
+					document.getElementById("listaSalas").insertAdjacentHTML('beforeend', '<li id="id_'+alquileres.rows[i].id+'">Alquiler '+alquileres.rows[iS].id+': '+alquileres.rows[i].fechaIni+'-'+alquileres.rows[i].fechaFin+' '+alquileres.rows[i].comentario+' '+
+                     '<a href="javascript:deleteAlquileres('+alquileres.rows[i].id+')class="ui-btn ui-btn-inline ui-btn-raised waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-delete zmd-2x"></i></a> ' +
+                     '<a href="javascript:verDetallesAlquileres('+alquileres.rows[i].id+')class="ui-btn ui-mini ui-btn-inline ui-btn-icon-block waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-edit zmd-2x"></i></a> ' +  
+                     '</li>');
+			};
+		}
+		else if(xhr.readyState == 4 && xhr.status == 403)
+		{
+			document.location.href = "movil.html#principal";
+		}
+	}
+	xhr.setRequestHeader('Authorization', 'Barer '+localStorage.getItem("token"));
+	xhr.setRequestHeader('Content-Type', 'aplication/x-www-form-urlencoded');
+	xhr.send();
+}
+
 //<a href="#" class="ui-btn ui-btn-inline ui-btn-raised waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-plus zmd-2x"></i></a>
 //<a href="#" class="ui-btn ui-btn-inline ui-btn-raised waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-delete zmd-2x"></i></a>
 //<a href="#" class="ui-btn ui-mini ui-btn-inline ui-btn-icon-block waves-effect waves-button waves-effect waves-button"><i class="zmdi zmdi-edit zmd-2x"></i></a>
